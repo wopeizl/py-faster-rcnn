2a9fc67a69d5c6542b2ab1b62ffed1f0ebd4e2f2
peizhilin <wopeizl@icloud.com>
2017-05-12 16:24:41 +0800

    initial v0.1

---
+++ shell/faster_rcnn_2class_train.sh
@@ -0,0 +1,2 @@
+nohup /data/zhangyuanpeng/ypzhang/workspace/caffe/py-faster-rcnn/experiments/scripts/faster_rcnn_end2end_2class.sh 4 VGG16 pascal_voc --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400,500,600,700]"&>log.txt&
+￼
